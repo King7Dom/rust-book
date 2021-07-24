8b2e1275d2ef5a68db76fda9526c9e6b4224927d
Dominic Cheung <dom.with.a.inic@gmail.com>
2021-07-25 09:07:13 +1000

    [ch2] Compare user input with generated number

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    // Generate the secret number
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is {}", secret_number);
 
+    // Get user input
     println!("Please input your guess");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    // Convert user input to a u32
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
 
     println!("You guessed: {}", guess);
+
+    // Compare and match the user input with the secret number
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
